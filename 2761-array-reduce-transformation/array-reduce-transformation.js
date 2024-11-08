4dd32686cff5c87798eb900a66ba43d35af1822a
nivas_pravin75 <51258356+Nivaspraveen@users.noreply.github.com>
2024-11-09 05:10:25 +0530

    Time: 50 ms (78.21%) | Memory: 49.6 MB (24.11%) - LeetSync

--- 2761-array-reduce-transformation/array-reduce-transformation.js
+++ 2761-array-reduce-transformation/array-reduce-transformation.js
@@ -5,5 +5,8 @@
  * @return {number}
  */
 var reduce = function(nums, fn, init) {
-    return nums.reduce(fn, init);
+    // return nums.reduce(fn, init);
+    let result = init;
+    for (let i = 0; i < nums.length; i++) result = fn(result, nums[i]);
+    return result;
 };
