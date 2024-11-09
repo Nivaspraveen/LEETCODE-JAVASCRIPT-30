3f08cba3c053bcaecf6965d52e6eeef06474bc63
nivas_pravin75 <51258356+Nivaspraveen@users.noreply.github.com>
2024-11-09 05:52:49 +0530

    Time: 53 ms (50.56%) | Memory: 49.6 MB (33.56%) - LeetSync

--- 2864-is-object-empty/is-object-empty.js
+++ 2864-is-object-empty/is-object-empty.js
@@ -3,5 +3,6 @@
  * @return {boolean}
  */
 var isEmpty = function(obj) {
-    return Object.keys(obj).length === 0;
+    if (Array.isArray(obj)) return obj.length === 0;
+    else return Object.keys(obj).length === 0;
 };
